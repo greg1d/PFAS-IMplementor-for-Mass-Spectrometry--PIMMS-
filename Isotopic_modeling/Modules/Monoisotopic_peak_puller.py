b7e7d0733afbda1d6a05d52d8e3a50b3b1e29eaf
Greg Kudzin <gregorykudzin@gmail.com>
2024-11-26 06:47:12 -0500

    Updated requirements

--- Isotopic_modeling/Modules/Monoisotopic_peak_puller.py
+++ Isotopic_modeling/Modules/Monoisotopic_peak_puller.py
@@ -65,15 +65,3 @@ def find_related_peaks_in_csv(file_path, z_range, M_range, mass_error_ppm):
                     break  # Break the outer loop if a match is found
     print(f"Number of iterations: {iteration_count}")
     return related_peaks
-
-
-if __name__ == "__main__":
-    file_path = "data/Dummy test blank subtracted data.csv"  # Update this path to your local CSV file
-    z_range = range(1, 100)  # This will check for z = 1 to 99
-    M_range = range(1, 11)  # This will check for M = 1 to 10
-    mass_error_ppm = 10  # Define the mass error in ppm
-
-    related_peaks = find_related_peaks_in_csv(
-        file_path, z_range, M_range, mass_error_ppm
-    )
-    print(f"Related peaks: {related_peaks}")
