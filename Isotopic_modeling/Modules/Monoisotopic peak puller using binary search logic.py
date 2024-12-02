1b8bd11f40ed3fc10f1f4a0f774a9731a6b1e138
Greg Kudzin <gregorykudzin@gmail.com>
2024-12-02 11:48:41 -0500

    Passed initial test

--- Isotopic_modeling/Modules/Monoisotopic peak puller using binary search logic.py
+++ Isotopic_modeling/Modules/Monoisotopic peak puller using binary search logic.py
@@ -23,6 +23,32 @@ def find_peaks_within_bounds(array, z, M, i, mass_error_ppm=10):
     return peaks_within_bounds, j_end - j_start
 
 
+def expand_group(array, initial_peak, z_range, M_range, mass_error_ppm=10):
+    group = [initial_peak]
+    identified_features = set(group)
+    i = array.index(initial_peak)
+
+    for z in z_range:
+        for M in M_range:
+            peaks, _ = find_peaks_within_bounds(array, z, M, i, mass_error_ppm)
+            for peak in peaks:
+                if peak not in identified_features:
+                    identified_features.add(peak)
+                    group.append(peak)
+                    # Continue expanding the group with the new peak
+                    for new_z in z_range:
+                        for new_M in M_range:
+                            new_peaks, _ = find_peaks_within_bounds(
+                                array, new_z, new_M, array.index(peak), mass_error_ppm
+                            )
+                            for new_peak in new_peaks:
+                                if new_peak not in identified_features:
+                                    identified_features.add(new_peak)
+                                    group.append(new_peak)
+
+    return group
+
+
 # Example usage
 array = [102, 103, 104, 105, 106, 107]
 z_range = range(1, 11)  # User-defined range for z from 1 to 10
@@ -31,6 +57,12 @@ M_range = range(1, 11)  # User-defined range for M from 1 to 10
 total_calculations = 0
 identified_features = set()
 groups = []
+
+for i in range(len(array)):
+    if array[i] not in identified_features:
+        group = expand_group(array, array[i], z_range, M_range)
+        groups.append(group)
+        identified_features.update(group)
 
 print("Groups of related peaks:")
 for group in groups:
