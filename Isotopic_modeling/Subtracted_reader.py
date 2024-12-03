8cf6a2945eebdfda8431433a512c99cdfea5addf
Greg Kudzin <gregorykudzin@gmail.com>
2024-12-03 11:30:34 -0500

    Fixed bug

--- Isotopic_modeling/Subtracted_reader.py
+++ Isotopic_modeling/Subtracted_reader.py
@@ -1,8 +1,5 @@
-import time
-
 import pandas as pd
 from Modules.binary_logic_puller import analyze_peaks
-from tqdm import tqdm
 
 
 def main():
@@ -12,25 +9,12 @@ def main():
 
     # Load the data to get RT, CCS, and ID values
     data_df = pd.read_csv(file_path)
-    rt_ccs_mapping = (
-        data_df.groupby("m/z")[["RT", "CCS", "ID"]]
-        .agg(lambda x: x.iloc[0])
-        .to_dict("index")
+    rt_ccs_mapping = data_df.set_index("ID").to_dict("index")
+
+    # Run the operation
+    groups, unrelated_features, total_calculations = analyze_peaks(
+        file_path, z_range, mass_error_ppm
     )
-
-    # Start the progress bar
-    with tqdm(total=100, desc="Processing peaks") as pbar:
-        start_time = time.time()
-
-        groups, unrelated_features, total_calculations = analyze_peaks(
-            file_path, z_range, mass_error_ppm
-        )
-
-        # Simulate progress update
-        pbar.update(100)
-
-        end_time = time.time()
-        elapsed_time = end_time - start_time
 
     # Print the number of groups identified
     print(f"Number of groups identified: {len(groups)}")
@@ -46,23 +30,23 @@ def main():
     # Print the number of calculations performed
     print(f"Number of calculations performed: {total_calculations}")
 
-    print(f"Script completed in {elapsed_time:.2f} seconds")
+    print("Script completed")
 
     # Export the results to a CSV file
     results = []
     for idx, group in enumerate(groups):
-        for peak in group:
-            # Extract RT, CCS, and ID values from the mapping
-            rt_value = rt_ccs_mapping.get(peak, {}).get("RT", "N/A")
-            ccs_value = rt_ccs_mapping.get(peak, {}).get("CCS", "N/A")
-            id_value = rt_ccs_mapping.get(peak, {}).get("ID", "N/A")
+        for peak, peak_id in group:
+            # Extract RT, CCS, and ID values from the mapping using the ID
+            rt_value = rt_ccs_mapping.get(peak_id, {}).get("RT", "N/A")
+            ccs_value = rt_ccs_mapping.get(peak_id, {}).get("CCS", "N/A")
+            id_value = rt_ccs_mapping.get(peak_id, {}).get("ID", "N/A")
             results.append(
                 {
                     "Group": idx + 1,
                     "Peak": peak,
+                    "ID": peak_id,
                     "RT": rt_value,
                     "CCS": ccs_value,
-                    "ID": id_value,
                 }
             )
 
