edd4f4b7497fa8fc7cc66e34c823d1d4eb75c213
Greg Kudzin <gregorykudzin@gmail.com>
2024-12-06 11:27:16 -0500

    Working drag and drop and highlighting

--- modules/drag_drop_widget.py
+++ modules/drag_drop_widget.py
@@ -10,19 +10,38 @@ class DragDropWidget(QListWidget):
         self.setSelectionMode(QListWidget.SelectionMode.SingleSelection)
         self.setMouseTracking(True)  # Enable mouse tracking
         self.itemEntered.connect(self.highlight_item)  # Connect the itemEntered signal
+        print("DragDropWidget initialized")
 
     def dragEnterEvent(self, event: QDragEnterEvent):
+        print("dragEnterEvent called")
         if event.mimeData().hasUrls():
             event.acceptProposedAction()
+            print("Drag event accepted")
+        else:
+            print("Drag event not accepted")
+
+    def dragMoveEvent(self, event: QDragEnterEvent):
+        print("dragMoveEvent called")
+        if event.mimeData().hasUrls():
+            event.acceptProposedAction()
+            print("Drag move event accepted")
+        else:
+            print("Drag move event not accepted")
 
     def dropEvent(self, event: QDropEvent):
-        files = [url.toLocalFile() for url in event.mimeData().urls()]
-        for file in files:
-            item = QListWidgetItem(file)
-            self.addItem(item)
-        print("Dropped files:", files)
+        print("dropEvent called")
+        if event.mimeData().hasUrls():
+            files = [url.toLocalFile() for url in event.mimeData().urls()]
+            for file in files:
+                item = QListWidgetItem(file)
+                self.addItem(item)
+            print("Dropped files:", files)
+            event.acceptProposedAction()
+        else:
+            print("Drop event not accepted")
 
     def contextMenuEvent(self, event):
+        print("contextMenuEvent called")
         context_menu = QMenu(self)
         remove_action = QAction("Remove", self)
         remove_action.triggered.connect(self.remove_selected_item)
@@ -30,10 +49,14 @@ class DragDropWidget(QListWidget):
         context_menu.exec(event.globalPos())
 
     def remove_selected_item(self):
+        print("remove_selected_item called")
         for item in self.selectedItems():
             self.takeItem(self.row(item))
+        print("Selected item removed")
 
     def highlight_item(self, item):
+        print("highlight_item called")
         for i in range(self.count()):
             self.item(i).setBackground(Qt.GlobalColor.white)  # Reset background color
         item.setBackground(Qt.GlobalColor.lightGray)  # Highlight the hovered item
+        print("Item highlighted:", item.text())
