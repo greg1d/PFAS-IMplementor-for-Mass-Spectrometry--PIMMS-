f4df8de5e3448a64dbd847fdeba1eceaed387eb2
Greg Kudzin <gregorykudzin@gmail.com>
2024-12-13 09:42:26 -0500

    Configured the align peaks algorithm to be independent

--- modules/Align_peaks_algorithm.py
+++ modules/Align_peaks_algorithm.py
@@ -16,6 +16,31 @@ def align_peaks_algorithm(rt_value, ccs_value, mz_value, file_paths):
         except Exception as e:
             print(f"Error reading file {file_path}: {e}")  # Debugging statement
 
+    # Perform merge sort based on the m/z column for every file
+    sorted_data_frames = []
+    for df in data_frames:
+        sorted_df = df.sort_values(by="m/z")
+        sorted_data_frames.append(sorted_df)
+        print(
+            f"Sorted DataFrame based on m/z:\n{sorted_df.head()}"
+        )  # Print the sorted DataFrame
+
     # Implement your complex peak alignment algorithm here
     # This is just a placeholder for the actual algorithm
     pass
+
+
+def main():
+    # Example list of files to process for the sake of ease
+    file_paths = [
+        r"F:\PFAS-IMplementor-for-Mass-Spectrometry--PIMMS-\.temp\260 B4 MB-1.d.DeMP.feather",
+        r"F:\PFAS-IMplementor-for-Mass-Spectrometry--PIMMS-\.temp\221 B3 16563.d.DeMP.feather",
+    ]
+    rt_value = "1.0"
+    ccs_value = "2.0"
+    mz_value = "3.0"
+    align_peaks_algorithm(rt_value, ccs_value, mz_value, file_paths)
+
+
+if __name__ == "__main__":
+    main()
