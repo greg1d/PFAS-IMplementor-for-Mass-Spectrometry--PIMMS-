54967d4005e534815997d61d780ff22f1561a3aa
Greg Kudzin <gregorykudzin@gmail.com>
2025-02-19 14:24:51 -0500

    Working with the main app now

--- PIMMS v1.2/IMspector Gadget/data_processing.py
+++ PIMMS v1.2/IMspector Gadget/data_processing.py
@@ -1,12 +1,12 @@
 import pandas as pd
-from config import DATA_FILE, STANDARDS_FILE
+from config import FILE_PATH, STANDARDS_FILE
 
 
 def load_adjusted_data():
     """Loads the main dataset."""
     try:
-        print(f"[INFO] Loading data from: {DATA_FILE}")
-        return pd.read_csv(DATA_FILE)
+        print(f"[INFO] Loading data from: {FILE_PATH}")
+        return pd.read_csv(FILE_PATH)
     except Exception as e:
         print(f"[ERROR] Could not load dataset: {e}")
         return pd.DataFrame()
