587cbe0cf61334b715ab802f657cdefaf1cc303d
Gregory Kudzin <gregorykudzin@gmail.com>
2025-02-28 13:20:40 -0500

    Checked and still opens even when no groups are selected

--- PIMMS v1.2/IMspector Gadget/rt_v_mz_library_search_modules/rt_v_mz_library_searcher.py
+++ PIMMS v1.2/IMspector Gadget/rt_v_mz_library_search_modules/rt_v_mz_library_searcher.py
@@ -588,7 +588,7 @@ def rt_vs_mz_plotly(m_z_RT_groups):
 
 def main():
     stacked_df = stack_library_with_adjusted()
-    mass_groups = mz_repeating_unit_analysis(stacked_df, selected_repeating_units)
+    mass_groups = mz_repeating_unit_analysis(stacked_df, selected_repeating_units={})
     split_mass_groups = split_mass_groups_by_groupid(mass_groups)
     sig_groups, messy_groups = rt_vs_mz_trend_analysis(split_mass_groups)
     refined_sig_groups = refine_messy_rt_groups(messy_groups)
