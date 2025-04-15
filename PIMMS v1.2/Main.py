5d823a9055df67056ede6318df174d086f0f389f
Greg Kudzin <gregorykudzin@gmail.com>
2025-04-15 13:24:26 -0400

    Updated main with added neutral loss checker and adduct filter

--- PIMMS v1.2/Main.py
+++ PIMMS v1.2/Main.py
@@ -49,6 +49,7 @@ from Standard_library_scoring import (  # Import PFAS and External Library match
 
 from single_chromatography import combined_filter_pipeline
 from adduct_checker import find_matching_mass_relationships
+from neutral_loss_checker import find_neutral_loss_matches
 
 
 def main():
@@ -421,7 +422,7 @@ def main():
         print(f"[ERROR] Failed to perform regression analysis: {e}")
         sys.exit(1)
 
-    print("[INFO] Applying post adduct filter...")
+    print("[INFO] Applying adduct filter...")
     try:
         adjusted_df = find_matching_mass_relationships(adjusted_df)
         adjusted_df.to_csv("PIMMS Validation work/PIMMS data/after_adduct_filter.csv")
@@ -434,6 +435,22 @@ def main():
         )
     except Exception as e:
         print(f"[ERROR] Failed to perform adduct filtering: {e}")
+        sys.exit(1)
+
+    try:
+        adjusted_df = find_neutral_loss_matches(adjusted_df)
+        adjusted_df.to_csv(
+            "PIMMS Validation work/PIMMS data/after_neutral_loss_filter.csv"
+        )
+
+        group_avg, group_std = count_non_zero_rows(adjusted_df)
+        print(
+            f"[INFO] After Applying adduct filter:\n"
+            f"  Average Non-Zero Rows: {group_avg}\n"
+            f"  Std Dev of Non-Zero Rows: {group_std}"
+        )
+    except Exception as e:
+        print(f"[ERROR] Failed to perform neutral loss filtering: {e}")
         sys.exit(1)
 
     output_path = "PIMMS v1.2/Data_output/PIMMS Processed Data set.csv"
